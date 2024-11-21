cfc5f9dd6624dee4aaa27ff3a5555e1516f23a05
Dhvani Mistry <dm3786@cumc.columbia.edu>
2024-11-21 17:09:42 -0500

    made 3 types of docs: dr, pt_english, and pt_spanish

--- prettier.py
+++ prettier.py
@@ -18,14 +18,14 @@ today = datetime.today().strftime("%Y-%m-%d")
 mrns = dict()
 
 
-def set_table_header_bg_color(cell):
+def set_bg_color(cell, color):
     """
     set background shading for Header Rows
     """
     tblCell = cell._tc
     tblCellProperties = tblCell.get_or_add_tcPr()
     clShading = OxmlElement('w:shd')
-    clShading.set(qn('w:fill'), "cce0eb") 
+    clShading.set(qn('w:fill'), color) 
     tblCellProperties.append(clShading)
     return cell
 
@@ -56,208 +56,245 @@ with open("matches/matches_" + today + ".txt", newline='') as csvfile:
 mrns = list(mrns.values())
 #print(mrns)
 
+doc_types = ['DR', "PT_ENGLISH", "PT_SPANISH"]
+
 for pt in mrns:
-    doc = docx.Document()
+    for doc_type in doc_types:
+        doc = docx.Document()
 
-    thistype = -1
+        thistype = -1
 
-    # mrn stage 
-    p = doc.add_paragraph()
+        # mrn stage 
+        p = doc.add_paragraph()
 
-    pt_name = p.add_run(pt['pt_name'])
-    pt_name.font.size = Pt(16)
+        pt_name = p.add_run(pt['pt_name'])
+        pt_name.font.size = Pt(16)
 
-    pt_name.add_break()
+        pt_name.add_break()
 
-    mrn_title = p.add_run("MRN: ")
-    mrn_title.bold = True
-    #mrn_title.add_tab()
-    mrn_num = p.add_run(pt['mrn'])
-    mrn_num.font.size = Pt(14)
+        mrn_title = p.add_run("MRN: ")
+        mrn_title.bold = True
+        #mrn_title.add_tab()
+        mrn_num = p.add_run(pt['mrn'])
+        mrn_num.font.size = Pt(14)
 
-    mrn_num.add_break()
+        mrn_num.add_break()
 
-    stage_title = p.add_run("Stage: ")
-    stage_title.bold = True
+        stage_title = p.add_run("Stage: ")
+        stage_title.bold = True
 
-    if pt['pt_stage'] != '':
-        stage = p.add_run(pt['pt_stage'])
-        stage.font.size = Pt(14)
-    else:
-        p.add_run("N/A")
-
-    # gene
-    p = doc.add_paragraph()
-    gene_title = p.add_run("Relevant Biomarkers:")
-    gene_title.bold = True
-    gene_title.add_break()
-
-    for gene in sorted(pt['pt_genes']):
-        genes = p.add_run(gene)
-        genes.font.size = docx.shared.Pt(16)
-        genes.add_break()
-
-    
-    doc = add_intro_text(doc)
-
-    p_studies_title = doc.add_paragraph()
-    p_studies_title.add_run("POTENTIAL TRIAL MATCHES").bold = True
-    
-    # iterate through studies
-    for row in pt['studies']:
-
-        #type
-        """
-        if(row['type'] != thistype):
-            p_type = doc.add_paragraph()
-            thistype = row['type']
-            #p_type.style.font.italic = True
-            if(row['type'] == '1'):
-                run = p_type.add_run("Tier " + row['type'] + ": Targeted Therapy")    
-            elif (row['type'] == '2'):
-                run = p_type.add_run("Tier " + row['type'] + ": Immunotherapy")
-            elif (row['type'] == '3'):
-                run = p_type.add_run("Tier " + row['type'] + ": Antibody/Drug Conjugate")
-            elif (row['type'] == '4'):
-                run = p_type.add_run("Tier " + row['type'] + ": All Others")
-        """
-                  
-        table = doc.add_table(rows=3, cols=3)
-
-        # table header (nct and therapy type / resectable stuff)
-        r = table.rows[0].cells
-        
-        r[0].text = ""
-        nct = r[0].paragraphs[0].add_run(row['nct_number'])
-        nct.bold = True
-        nct.font.size = Pt(16)
-        
-
-        r[1].text = ""
-        tt = r[1].paragraphs[0].add_run("THERAPY TYPE: ") 
-        tt.bold = True
-        tt.font.size = Pt(10)
-        r[1].paragraphs[0].add_run(row['therapy_type'])
-
-        # early_stage_resectable, early_stage_unresectable, advanced_first_line, advanced_second_line
-        therapy_stages = []
-
-        if row['early_stage_resectable'] == "Y":
-            therapy_stages.append("Early Stage Resectable")
-        if row['early_stage_unresectable'] == "Y":
-            therapy_stages.append("Early Stage Unresectable")
-        if row['advanced_first_line'] == "Y":
-            therapy_stages.append("Advanced First Line")
-        if row['advanced_second_line'] == "Y":
-            therapy_stages.append("Advanced Second Line")
-        
-        """
-        if len(therapy_stages) != 0:
-            r[1].paragraphs[0].add_run().add_break()
-            r[1].paragraphs[0].add_run( ", ".join(therapy_stages) )
-        """
-
-        r[1].paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
-        
-        r[1].merge(r[2])
-
-        set_table_header_bg_color(r[0])
-        set_table_header_bg_color(r[1])
-        set_table_header_bg_color(r[2])
-
-        # row 2 title of study
-        
-        r = table.rows[1].cells
-        r[0].text = "" 
-        r[0].paragraphs[0].add_run(row['title']).italic = True
-        r[0].merge(r[1])
-        r[0].merge(r[2])
-
-        # row 3: match reason and trial disease stages
-
-        r = table.rows[2].cells
-
-
-        r[0].text = ""
-        reason = r[0].paragraphs[0].add_run('REASON FOR MATCH: ')
-        reason.bold = True
-        reason.font.size = Pt(10)
-        if (row['trial_keyword1'] == "N/A" or row['trial_keyword1'] == ""):
-            r[0].paragraphs[0].add_run("N/A")
+        if pt['pt_stage'] != '':
+            stage = p.add_run(pt['pt_stage'])
+            stage.font.size = Pt(14)
         else:
-            r[0].paragraphs[0].add_run(row['trial_keyword1'] + ' ' + row['trial_keyword_2']).font.size = Pt(14)
+            p.add_run("N/A")
 
-        r[0].paragraphs[0].add_run().add_break()
+        # gene
+        p = doc.add_paragraph()
+        gene_title = p.add_run("Relevant Biomarkers:")
+        gene_title.bold = True
+        gene_title.add_break()
 
-        strength = r[0].paragraphs[0].add_run("STRENGTH OF MATCH: ")
-        strength.bold = True
-        strength.font.size = Pt(10)
+        for gene in sorted(pt['pt_genes']):
+            genes = p.add_run(gene)
+            genes.font.size = docx.shared.Pt(16)
+            genes.add_break()
 
-        if row['type'] == "1":
-            r[0].paragraphs[0].add_run("Excellent")
-        elif row['type'] == "2":
-            r[0].paragraphs[0].add_run("Good")
-        elif row['type'] == "3":
-            r[0].paragraphs[0].add_run("Possible")
-        else:
-            r[0].paragraphs[0].add_run("Other")
-
-        r[0].paragraphs[0].add_run().add_break()
-
-        dis_stages = r[0].paragraphs[0].add_run("DISEASE STAGES CONSIDERED: ")
-        dis_stages.bold = True
-        dis_stages.font.size = Pt(10)
-        if len(therapy_stages) == 0:
-            r[0].paragraphs[0].add_run("N/A")
-        else:
-            r[0].paragraphs[0].add_run(", ".join(therapy_stages)).font.size = Pt(12)
-
-        r[0].merge(r[1])
-        r[0].merge(r[2])
-
-        doc.add_paragraph()
-
-    
-    # add borders, center align, line spacing, overall font
-
-    for t in doc.tables:
-        tbl = t._tbl
-
-        for cell in tbl.iter_tcs():
-            tcPr = cell.tcPr
-            tcBorders = OxmlElement("w:tcBorders")
-
-            left = OxmlElement("w:left")
-            left.set(qn("w:val"), "none")
-
-            right = OxmlElement("w:right")
-            right.set(qn("w:val"), "none")
-
-            top = OxmlElement("w:top")
-            top.set(qn("w:val"), "none")
-
-            bottom = OxmlElement("w:bottom")
-            bottom.set(qn("w:val"), "single")
-            bottom.set(qn("w:sz"), "4")
-            bottom.set(qn("w:color"), "#c2c2c2")
-
-            tcBorders.append(top)
-            tcBorders.append(left)
-            tcBorders.append(bottom)
-            tcBorders.append(right)
-            tcPr.append(tcBorders)
-
-        for r in range(len(table.rows)):
-            for c in range(len(table.columns)):
-                t.cell(r,c).vertical_alignment = WD_ALIGN_VERTICAL.CENTER
-                pt_num = 3
-                t.cell(r,c).paragraphs[0].paragraph_format.space_after = Pt(pt_num)
-                t.cell(r,c).paragraphs[0].paragraph_format.space_before = Pt(pt_num)
-
-    for paragraph in doc.paragraphs:
-        paragraph.style.font.name = "Aptos"
-        paragraph.style.font.size = Pt(12)
-    
-   
         
-    doc.save("matches/" + pt['mrn'] + "_matchlist.docx")
+        doc = add_intro_text(doc, doc_type)
+
+        p_studies_title = doc.add_paragraph()
+        p_studies_title.add_run("POTENTIAL TRIAL MATCHES").bold = True
+        
+        # iterate through studies
+        for row in pt['studies']:
+
+            if (doc_type == "PT_SPANISH"):
+                if (row['spanish title'] == ""):
+                    continue
+
+            #type
+            """
+            if(row['type'] != thistype):
+                p_type = doc.add_paragraph()
+                thistype = row['type']
+                #p_type.style.font.italic = True
+                if(row['type'] == '1'):
+                    run = p_type.add_run("Tier " + row['type'] + ": Targeted Therapy")    
+                elif (row['type'] == '2'):
+                    run = p_type.add_run("Tier " + row['type'] + ": Immunotherapy")
+                elif (row['type'] == '3'):
+                    run = p_type.add_run("Tier " + row['type'] + ": Antibody/Drug Conjugate")
+                elif (row['type'] == '4'):
+                    run = p_type.add_run("Tier " + row['type'] + ": All Others")
+            """
+
+            # row for description not included in doc version
+            table_rows = 4
+
+            if doc_type == "DR":
+                table_rows=3
+
+            table = doc.add_table(rows=table_rows, cols=3)
+
+            # table header (nct and therapy type / resectable stuff)
+            r = table.rows[0].cells
+            
+            r[0].text = ""
+            nct = r[0].paragraphs[0].add_run(row['nct_number'])
+            nct.bold = True
+            nct.font.size = Pt(16)
+            
+
+            r[1].text = ""
+            tt = r[1].paragraphs[0].add_run("THERAPY TYPE: ") 
+            tt.bold = True
+            tt.font.size = Pt(10)
+            r[1].paragraphs[0].add_run(row['therapy_type'])
+
+            # early_stage_resectable, early_stage_unresectable, advanced_first_line, advanced_second_line
+            therapy_stages = []
+
+            if row['early_stage_resectable'] == "Y":
+                therapy_stages.append("Early Stage Resectable")
+            if row['early_stage_unresectable'] == "Y":
+                therapy_stages.append("Early Stage Unresectable")
+            if row['advanced_first_line'] == "Y":
+                therapy_stages.append("Advanced First Line")
+            if row['advanced_second_line'] == "Y":
+                therapy_stages.append("Advanced Second Line")
+            
+            """
+            if len(therapy_stages) != 0:
+                r[1].paragraphs[0].add_run().add_break()
+                r[1].paragraphs[0].add_run( ", ".join(therapy_stages) )
+            """
+
+            r[1].paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+            
+            r[1].merge(r[2])
+
+            set_bg_color(r[0], 'cce0eb')
+            set_bg_color(r[1], 'cce0eb')
+            set_bg_color(r[2], 'cce0eb')
+
+            # row 2 title of study
+            
+            r = table.rows[1].cells
+            r[0].text = "" 
+            if doc_type == "PT_SPANISH":       
+                r[0].paragraphs[0].add_run(row['spanish title']).italic = True
+            else:
+                r[0].paragraphs[0].add_run(row['title']).italic = True
+            r[0].merge(r[1])
+            r[0].merge(r[2])
+
+            # row 3: match reason and trial disease stages
+
+            r = table.rows[2].cells
+
+
+            r[0].text = ""
+            reason = r[0].paragraphs[0].add_run('REASON FOR MATCH: ')
+            reason.bold = True
+            reason.font.size = Pt(10)
+            if (row['trial_keyword1'] == "N/A" or row['trial_keyword1'] == ""):
+                r[0].paragraphs[0].add_run("N/A")
+            else:
+                r[0].paragraphs[0].add_run(row['trial_keyword1'] + ' ' + row['trial_keyword_2']).font.size = Pt(14)
+
+            r[0].paragraphs[0].add_run().add_break()
+
+            strength = r[0].paragraphs[0].add_run("STRENGTH OF MATCH: ")
+            strength.bold = True
+            strength.font.size = Pt(10)
+
+            if row['type'] == "1":
+                r[0].paragraphs[0].add_run("Excellent")
+            elif row['type'] == "2":
+                r[0].paragraphs[0].add_run("Good")
+            elif row['type'] == "3":
+                r[0].paragraphs[0].add_run("Possible")
+            else:
+                r[0].paragraphs[0].add_run("Other")
+
+            r[0].paragraphs[0].add_run().add_break()
+
+            dis_stages = r[0].paragraphs[0].add_run("DISEASE STAGES CONSIDERED: ")
+            dis_stages.bold = True
+            dis_stages.font.size = Pt(10)
+            if len(therapy_stages) == 0:
+                r[0].paragraphs[0].add_run("N/A")
+            else:
+                r[0].paragraphs[0].add_run(", ".join(therapy_stages)).font.size = Pt(12)
+
+            r[0].merge(r[1])
+            r[0].merge(r[2])
+
+            # row 4: description IF not dr format
+
+            if doc_type != "DR":
+
+                r = table.rows[3].cells
+                r[0].text = "" 
+                if doc_type == "PT_SPANISH":       
+                    r[0].paragraphs[0].add_run(row['spanish description'])
+                else:
+                    r[0].paragraphs[0].add_run(row['english description'])
+                r[0].merge(r[1])
+                r[0].merge(r[2])
+
+                set_bg_color(r[0], 'f7f7f7')
+                set_bg_color(r[1], 'f7f7f7')
+                set_bg_color(r[2], 'f7f7f7')
+
+
+
+
+
+            doc.add_paragraph()
+
+        
+        # add borders, center align, line spacing, overall font
+
+        for t in doc.tables:
+            tbl = t._tbl
+
+            for cell in tbl.iter_tcs():
+                tcPr = cell.tcPr
+                tcBorders = OxmlElement("w:tcBorders")
+
+                left = OxmlElement("w:left")
+                left.set(qn("w:val"), "none")
+
+                right = OxmlElement("w:right")
+                right.set(qn("w:val"), "none")
+
+                top = OxmlElement("w:top")
+                top.set(qn("w:val"), "none")
+
+                bottom = OxmlElement("w:bottom")
+                bottom.set(qn("w:val"), "single")
+                bottom.set(qn("w:sz"), "4")
+                bottom.set(qn("w:color"), "#c2c2c2")
+
+                tcBorders.append(top)
+                tcBorders.append(left)
+                tcBorders.append(bottom)
+                tcBorders.append(right)
+                tcPr.append(tcBorders)
+
+            for r in range(len(table.rows)):
+                for c in range(len(table.columns)):
+                    t.cell(r,c).vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+                    pt_num = 3
+                    t.cell(r,c).paragraphs[0].paragraph_format.space_after = Pt(pt_num)
+                    t.cell(r,c).paragraphs[0].paragraph_format.space_before = Pt(pt_num)
+
+        for paragraph in doc.paragraphs:
+            paragraph.style.font.name = "Aptos"
+            paragraph.style.font.size = Pt(12)
+        
+    
+            
+        doc.save("matches/" + pt['mrn'] + "_matchlist_" + doc_type + ".docx")
--- report_intro_text.py
+++ report_intro_text.py
@@ -3,47 +3,91 @@ import docx
 import docx.document
 
 
-def add_intro_text(doc : docx.document.Document ):
+def add_intro_text(doc : docx.document.Document, doc_type ):
 
-    p_header = doc.add_paragraph()
-    title = p_header.add_run("DISRUPT :  PATIENT NOTIFICATION OF TRIAL MATCHING RESULTS")
-    title.bold = True
+    if doc_type == "PT_SPANISH":
+        p_header = doc.add_paragraph()
+        title = p_header.add_run("DISRUPT : NOTIFICACIÓN AL PACIENTE DE LOS RESULTADOS DE LA CONEXIÓN A ENSAYOS CLINICOS")
+        title.bold = True
 
-    p = doc.add_paragraph()
+        p = doc.add_paragraph()
 
-    title = p.add_run("How we made this report")
-    title.bold = True
-    title.add_break()
-    p.add_run("We looked at the information from your doctor about your cancer. This included details about the changes in your tumor, how your immune system is dealing with the tumor, and the stage of your tumor. We used this information to make a list of research studies at Columbia and other places that might be helpful for you and your treatment.")
+        title = p.add_run("Cómo hicimos este informe")
+        title.bold = True
+        title.add_break()
+        p.add_run("Analizamos la información dada por su médico sobre su cáncer. Esto incluía detalles sobre los cambios en el tumor, cómo su sistema inmunitario está lidiando con el tumor y el estadio del tumor. Utilizamos esta información para hacer una lista de estudios de investigación en Columbia y otros lugares que podrían ser útiles para usted y su tratamiento.")
 
-    p2 = doc.add_paragraph()
-    title = p2.add_run("How to use this report")
-    title.bold = True
-    title.add_break()
-    p2.add_run("This report was made to include all the trials you might be able to join. There might be many that don't really apply to you even if we list them. We use our system to organize trials based on which ones are most likely to be right for you. We put these matches into different groups:")
+        p2 = doc.add_paragraph()
+        title = p2.add_run("Cómo utilizar este informe ")
+        title.bold = True
+        title.add_break()
+        p2.add_run("Este informe se hizo para incluir todos los ensayos a los que podría unirse. Es posible que haya muchos que realmente no se apliquen a usted, incluso los enumeramos. Utilizamos nuestro sistema para organizar ensayos en función de cuáles tienen más probabilidades de ser adecuados para usted. Dividimos estos ensayos en diferentes grupos:")
 
-    doc.add_paragraph("Excellent Match: You have a change in one or more genes (called a mutation) in your tumor and there's a trial for people with the same mutation.",
-                       style='List Bullet')
-    doc.add_paragraph("Good Match: A new kind of treatment is being tested for your type of tumor, but it might not be tested for the specific change in your tumor genes (mutation).",
-                       style='List Bullet')
-    doc.add_paragraph("Possible Match: We're not sure if this trial is right for your tumor, but we want you and your doctor to know about it just in case.",
-                       style='List Bullet')
-    doc.add_paragraph("Other Match: A possible match that is hard for us to classify",
-                       style='List Bullet')
+        doc.add_paragraph("Compatibilidad excelente: hay un cambio en uno o más genes (llamado mutación) en el tumor y hay un ensayo para personas con la misma mutación.",
+                        style='List Bullet')
+        doc.add_paragraph("Compatibilidad Buena: se está probando un nuevo tipo de tratamiento para su tipo de tumor, pero es posible que no se pruebe para el cambio específico en los genes del tumor (mutación).",
+                        style='List Bullet')
+        doc.add_paragraph("Compatibilidad Posible: No estamos seguros de si este ensayo es adecuado para su tumor, pero queremos que usted y su médico lo sepan por si acaso.",
+                        style='List Bullet')
+        doc.add_paragraph("Otra compatibilidad: Una posible compatibilidad que nos cuesta clasificar.",
+                        style='List Bullet')
 
-    doc.add_paragraph("Even if a study seems like it could be a good fit for you, there might be other reasons why it's not the right choice. For example, it might be a good match for people with lung cancer and your specific genetic mutation, but only for those who have already had surgery, and your doctor might not think you're a candidate for surgery. The best way to use this report is to talk about anything you're curious about with your doctor, who will also receive a copy of this report.")
+        doc.add_paragraph("Incluso si un estudio parece ser una buena opción para usted, puede haber otras razones por las que no es la opción correcta. Por ejemplo, podría ser una buena opción para las personas con cáncer de pulmón y su mutación genética específica, pero solo para aquellas que ya se han sometido a una cirugía, y es posible que su médico piense que usted no es candidato/a para la cirugía. La mejor manera de usar este informe es hablar sobre cualquier cosa que le interese con su médico, quien también recibirá una copia de este informe.")
+        
+        p3 = doc.add_paragraph()
+        title = p3.add_run("¿Qué sucede si tengo preguntas sobre este informe?")
+        title.bold = True
+        title.add_break()
+        p3.add_run("La mejor persona para preguntar sobre esto es su médico. Hacemos todo lo posible para proporcionar información precisa, pero puede haber errores. Su médico es la mejor persona para confirmar esto. Además, la información puede cambiar con el tiempo, incluidas las versiones de pruebas disponibles. Hacemos todo lo posible para mantener esta información actualizada, pero es posible que su médico conozca ensayos que no se mencionan aquí.")
+
+        p4 = doc.add_paragraph()
+        title = p4.add_run("Cómo unirse a un ensayo clínico")
+        title.bold = True
+        title.add_break()
+        p4.add_run("Incluso si encontramos un buen ensayo para usted, es posible que tenga que cumplir con ciertos requisitos para participar en el ensayo y recibir el tratamiento. Cada ensayo es diferente, por lo que es importante hablar con su médico al respecto. Si bien hemos tratado de hacer que la información sea fácil de entender, siempre es mejor preguntarle a su médico si tiene alguna pregunta. Su médico puede proporcionarle la información más precisa y mantenerlo/la actualizado/a sobre cualquier nuevo desarrollo o ensayo que esté disponible con el tiempo.").add_break()
+      
+        return doc
     
-    p3 = doc.add_paragraph()
-    title = p3.add_run("What if I have questions about this report")
-    title.bold = True
-    title.add_break()
-    p3.add_run("The best person to ask about this is your doctor. We do our best to provide accurate information, but there might be mistakes. Your doctor is the best person to confirm this. Also, information can change over time, including available trials. We do our best to keep this information up to date, but your doctor may know about trials not listed here.")
+    else: 
+        p_header = doc.add_paragraph()
+        title = p_header.add_run("DISRUPT :  PATIENT NOTIFICATION OF TRIAL MATCHING RESULTS")
+        title.bold = True
 
-    p4 = doc.add_paragraph()
-    title = p4.add_run("How to join a trial")
-    title.bold = True
-    title.add_break()
-    p4.add_run("Even if we find a good trial for you, you may need to meet certain requirements to be part of the trial and receive the treatment. Each trial is different, so it's important to talk to your doctor about this. While we've tried to make the information easy to understand, it's always best to ask your doctor if you have any questions. Your doctor can provide you with the most accurate information and keep you updated on any new developments or trials that become available over time.").add_break()
+        p = doc.add_paragraph()
 
-    
-    return doc
+        title = p.add_run("How we made this report")
+        title.bold = True
+        title.add_break()
+        p.add_run("We looked at the information from your doctor about your cancer. This included details about the changes in your tumor, how your immune system is dealing with the tumor, and the stage of your tumor. We used this information to make a list of research studies at Columbia and other places that might be helpful for you and your treatment.")
+
+        p2 = doc.add_paragraph()
+        title = p2.add_run("How to use this report")
+        title.bold = True
+        title.add_break()
+        p2.add_run("This report was made to include all the trials you might be able to join. There might be many that don't really apply to you even if we list them. We use our system to organize trials based on which ones are most likely to be right for you. We put these matches into different groups:")
+
+        doc.add_paragraph("Excellent Match: You have a change in one or more genes (called a mutation) in your tumor and there's a trial for people with the same mutation.",
+                        style='List Bullet')
+        doc.add_paragraph("Good Match: A new kind of treatment is being tested for your type of tumor, but it might not be tested for the specific change in your tumor genes (mutation).",
+                        style='List Bullet')
+        doc.add_paragraph("Possible Match: We're not sure if this trial is right for your tumor, but we want you and your doctor to know about it just in case.",
+                        style='List Bullet')
+        doc.add_paragraph("Other Match: A possible match that is hard for us to classify",
+                        style='List Bullet')
+
+        doc.add_paragraph("Even if a study seems like it could be a good fit for you, there might be other reasons why it's not the right choice. For example, it might be a good match for people with lung cancer and your specific genetic mutation, but only for those who have already had surgery, and your doctor might not think you're a candidate for surgery. The best way to use this report is to talk about anything you're curious about with your doctor, who will also receive a copy of this report.")
+        
+        p3 = doc.add_paragraph()
+        title = p3.add_run("What if I have questions about this report")
+        title.bold = True
+        title.add_break()
+        p3.add_run("The best person to ask about this is your doctor. We do our best to provide accurate information, but there might be mistakes. Your doctor is the best person to confirm this. Also, information can change over time, including available trials. We do our best to keep this information up to date, but your doctor may know about trials not listed here.")
+
+        p4 = doc.add_paragraph()
+        title = p4.add_run("How to join a trial")
+        title.bold = True
+        title.add_break()
+        p4.add_run("Even if we find a good trial for you, you may need to meet certain requirements to be part of the trial and receive the treatment. Each trial is different, so it's important to talk to your doctor about this. While we've tried to make the information easy to understand, it's always best to ask your doctor if you have any questions. Your doctor can provide you with the most accurate information and keep you updated on any new developments or trials that become available over time.").add_break()
+
+        
+        return doc
